0b248308e412d2b78554aeff0b1ba29cdaa4e5e2
Hanna Ruter <hanna.ruter@gmail.com>
2024-09-18 09:02:07 +0200

    adding jest update

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,14 @@ import type { Config } from 'jest'
 
 const config: Config = {
   preset: 'ts-jest',
-  testEnvironment: 'jest-environment-jsdom' // Korrekt miljö
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.vue$': '@vue/vue3-jest'
+  },
+  testEnvironmentOptions: {
+    customExportConditions: ['node', 'node-addons']
+  }
 }
 
 export default config
